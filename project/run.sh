578416b988c8a6404c12eff05d35e505018f2057
Shruti Sharma <shruti.ss48@gmail.com>
2020-12-03 12:10:22 +0530

    Shruti's v2

--- project/run.sh
+++ project/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 bc=`echo -n $1 | rev | cut -d'.' -f 2- | rev`".bc"
-clang -c -emit-llvm $1 -o $bc
+clang -fno-discard-value-names -O0 -c -emit-llvm $1 -o $bc
 make
 opt -instnamer -load ./main.dylib -sign_analysis < $bc 1>/dev/null
 opt -dot-callgraph $bc
-dot -Tpng tests/sign_test1.bc.callgraph.dot -o e.png
+dot -Tpng $bc.callgraph.dot -o e.png
 #for linux : xdg-open e.png
 #windows : start e.png
 open e.png
